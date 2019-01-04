082d49186d9598aa6558333d23ae4a7c62866f2f
wongco <ginson@gmail.com>
2019-01-03 19:05:30 -0800

    add song api content

--- app/config.js
+++ app/config.js
@@ -1,3 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000';
+const APP_API_BASE_URL = 'http://localhost:3000';
+const SONG_API_BASE = 'https://jons-playlist-maker.herokuapp.com';
 
-export { API_BASE_URL };
+export { APP_API_BASE_URL, SONG_API_BASE };
--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,4 +1,4 @@
-import { API_BASE_URL } from '../app/config.js';
+import { APP_API_BASE_URL, SONG_API_BASE } from '../app/config.js';
 
 // callback to run after image file is uploaded
 export function previewFile() {
@@ -36,12 +36,29 @@ export function previewFile() {
   }
 }
 
-// function to retrieve info from API
+// callback for submit event - function to retrieve info from API
 async function getRelations(encodedImg) {
-  console.log(`Sending stuff to ${API_BASE_URL} via AJAX:`);
+  // get clarifai results
+  const relationsList = await getClarifaiResults(encodedImg);
+  console.log(relationsList);
+  const firstTerm = relationsList[0].association;
+
+  // get related song results
+  const songsResults = await getPlaylist(firstTerm);
+
+  const songsList = songsResults.playlist;
+  const queryInfo = songsResults.info;
+
+  // render to dom
+  updateDom(songsList, firstTerm, queryInfo);
+}
+
+// send clarifai base64 encoded img and get back list of relations
+async function getClarifaiResults(encodedImg) {
+  console.log(`Sending stuff to ${APP_API_BASE_URL} via AJAX:`);
   console.log(encodedImg);
 
-  const url = `${API_BASE_URL}/images`;
+  const url = `${APP_API_BASE_URL}/images/`;
   const method = 'POST';
   console.log(`API METHOD: ${method}`);
   console.log(`API URL: ${url}`);
@@ -56,5 +73,77 @@ async function getRelations(encodedImg) {
       console.log('Error sending base64 image to API!');
     }
   });
-  console.log(apiResponse);
+  return apiResponse.relations;
+}
+
+async function getPlaylist(name) {
+  const url = `${SONG_API_BASE}/song/`;
+  const method = 'GET';
+  console.log(`API METHOD: ${method}`);
+  console.log(`API URL: ${url}`);
+  const data = {
+    name
+  };
+  const apiResponse = await $.ajax({
+    url,
+    method,
+    data,
+    error: () => {
+      console.log('Error sending base64 image to API!');
+    }
+  });
+
+  return apiResponse;
+}
+
+// update Front End with results
+function updateDom(playlist, keyterm, queryInfo) {
+  console.log(keyterm);
+  console.log(playlist);
+  console.log(` Query Tag by jon: ${queryInfo}`);
+
+  // add code to send Query Info to Dom
+
+  const maxLength = playlist.length > 5 ? 5 : playlist.length;
+  for (let i = 0; i < maxLength; i++) {
+    const songObj = playlist[i];
+    const { song, artist, albumIMG } = songObj;
+    renderSongToDOM(song, artist, albumIMG);
+  }
+}
+
+function renderSongToDOM(song, artist, albumUrl) {
+  // const $songsContainer = $('#songs-container');
+
+  const songContainerEl = document.querySelector('#songs-container');
+  songContainerEl.classList.add('show');
+
+  const songDiv = document.createElement('div');
+  songDiv.classList.add('row', 'py-2', 'border', 'align-items-center');
+
+  const titleSpan = document.createElement('span');
+  titleSpan.classList.add('col-9', 'h4');
+  titleSpan.innerText = `${song}`;
+
+  const albumDiv = document.createElement('div');
+  albumDiv.classList.add('col-3', 'row', 'justify-content-end');
+
+  // image Element
+  const imageEl = document.createElement('img');
+  imageEl.setAttribute('src', albumUrl);
+
+  // artist name div
+  const artistDiv = document.createElement('div');
+  artistDiv.innerText = artist;
+
+  // append both to albumDiv
+  albumDiv.appendChild(imageEl);
+  albumDiv.appendChild(artistDiv);
+
+  // append title and album info to songDiv
+  songDiv.appendChild(titleSpan);
+  songDiv.appendChild(albumDiv);
+
+  // append content to songContainerEl
+  songContainerEl.appendChild(songDiv);
 }
